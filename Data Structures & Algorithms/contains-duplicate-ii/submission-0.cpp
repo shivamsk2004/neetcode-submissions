31f154b6567b84b89e68c93d6f98d4303a502282
Shivam Kumbhakarna <133688937+shivamsk2004@users.noreply.github.com>
2026-05-28 07:15:54 +0530

    Add: contains-duplicate-ii - submission-0

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++){
+            v.push_back({nums[i],i});
+        }
+        sort(v.begin(),v.end());
+        for(int i=1;i<n;i++){
+            if(v[i].first==v[i-1].first){
+                if(abs(v[i].second-v[i-1].second)<=k)return true;
+            }
+        }
+        return false;
+    }
+};
